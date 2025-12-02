012993504bb4be5f7fc691e3cf9a9875101aa091
raulwag <134857157+raulwag@users.noreply.github.com>
2025-12-02 10:34:37 -0300

    Update ClickSignTab.php

--- src/ClickSignTab.php
+++ src/ClickSignTab.php
@@ -1,1 +1,41 @@
+<?php
 
+namespace GlpiPlugin\Clicksign;
+
+use CommonGLPI;
+use Glpi\Application\View\TemplateRenderer;
+use Ticket;
+
+class ClickSignTab extends CommonGLPI
+{
+    static function getTypeName($nb = 0)
+    {
+        return __('Enviar para ClickSign', 'clicksign');
+    }
+
+    function getTabNameForItem(CommonGLPI $item, $withtemplate = 0)
+    {
+        if ($item instanceof Ticket) {
+            return self::getTypeName();
+        }
+        return '';
+    }
+
+    static function displayTabContentForItem(CommonGLPI $item, $tabnum = 1, $withtemplate = 0)
+    {
+        if ($item instanceof Ticket) {
+            self::showForTicket($item);
+        }
+        return true;
+    }
+
+    static function showForTicket(Ticket $ticket)
+    {
+        $config = \Config::getConfigurationValues('plugin:clicksign');
+        
+        TemplateRenderer::getInstance()->display('@clicksign/clicksign_tab.html.twig', [
+            'ticket' => $ticket,
+            'webhook_url' => $config['n8n_webhook_url'] ?? ''
+        ]);
+    }
+}
